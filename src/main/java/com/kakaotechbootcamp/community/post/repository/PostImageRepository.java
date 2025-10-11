b2eb49d08fe14fdb3f8737d9a896be89a4f3db78
유제혁 <wpgur07@gmail.com>
2025-10-11 23:04:32 +0900

    feat: postImage엔티티 클래스 작성, Repository클래스 작성
    - postImage관련 연관관계 설정 및 엔티티 작성
    - repository 클래스 틀 작성

---
+++ src/main/java/com/kakaotechbootcamp/community/post/repository/PostImageRepository.java
@@ -0,0 +1,8 @@
+package com.kakaotechbootcamp.community.post.repository;
+
+import com.kakaotechbootcamp.community.post.entity.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+
+}
