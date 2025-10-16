6f9af4d1140237d1960f93d2589c32ba151db78d
유제혁 <wpgur07@gmail.com>
2025-10-16 09:32:09 +0900

    chore: refresh 토큰 발급 로직 주석

--- src/main/java/com/kakaotechbootcamp/community/utils/security/JwtTokenProvider.java
+++ src/main/java/com/kakaotechbootcamp/community/utils/security/JwtTokenProvider.java
@@ -4,40 +4,59 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.time.Clock;
 import java.time.Instant;
 import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
-    private static final String USER_NAME = "username";
-    private final Key key;
-    private final long accessTtlMillis;
     private final Clock clock;
 
-    public JwtTokenProvider(
-        @Value("${security.jwt.secret}") String secret,
-        @Value("${security.jwt.access-ttl-millis}") long accessTtlMillis,
-        Clock clock
-    ) {
-        byte[] bytes = secret.getBytes(java.nio.charset.StandardCharsets.UTF_8);
-        if (bytes.length < 32) {
-            throw new IllegalArgumentException("시크릿 키 32글자 검증");
+    // 빈 생성시점에 정해지지 않는 것들
+    // 싱글톤 설정 클래스 작성 혹은 별도의 검증과정이 필요할 것 같음
+    private Key key;
+    @Value("#{environment['security.jwt.secret']}")
+    private String secret;
+    @Value("#{environment['security.jwt.refresh-ttl-millis']}")
+    private long refreshTtlMillis;
+    @Value("#{environment['security.jwt.access-ttl-millis']}")
+    private long accessTtlMillis;
+
+    @PostConstruct
+    private void init() {
+        if (secret == null || refreshTtlMillis == 0 || accessTtlMillis == 0) {
+            throw new BeanInitializationException("필수 값을 불러오지 못했습니다.");
         }
-        this.key = io.jsonwebtoken.security.Keys.hmacShaKeyFor(bytes);
-        this.accessTtlMillis = accessTtlMillis;
-        this.clock = clock;
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String createAccessToken(Long userId, String username) {
+    public String createAccessToken(Long userId) {
+        return createToken(userId, accessTtlMillis);
+    }
+
+    /**
+     * 남겨놓은 이유 : jwt로 할지 불투명 토큰으로 할지 아직까지도 못정해서 일단 두었습니다...
+     */
+    public String createRefreshToken(Long userId) {
+        return createToken(userId, refreshTtlMillis);
+    }
+
+    public String createToken(long userId, long accessTtlMillis) {
         Instant now = Instant.now(clock);
         return Jwts.builder()
             .setSubject(String.valueOf(userId))
-            .claim(USER_NAME, username)
             .setIssuedAt(Date.from(now))
             .setExpiration(new Date(now.toEpochMilli() + accessTtlMillis))
             .signWith(key, SignatureAlgorithm.HS256)
@@ -50,9 +69,5 @@ public class JwtTokenProvider {
 
     public Long userId(String token) {
         return Long.valueOf(parse(token).getBody().getSubject());
-    }
-
-    public String username(String token) {
-        return parse(token).getBody().get(USER_NAME, String.class);
     }
 }
